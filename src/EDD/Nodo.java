1907acd3958a4113656d71dea9e695974c2a9f56
Juan González <155971097+JuanGzlz@users.noreply.github.com>
2024-10-13 18:25:30 -0400

    2nd Commit

--- src/EDD/Nodo.java
+++ src/EDD/Nodo.java
@@ -9,5 +9,29 @@ package EDD;
  * @author Admin
  */
 public class Nodo {
+    private Nodo pNext;
+    private Object data;
+
+    public Nodo(Object data) {
+        this.pNext = null;
+        this.data = data;
+    }
+
+    public Nodo getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Nodo pNext) {
+        this.pNext = pNext;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    
     
 }
